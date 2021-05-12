914676c73b05c6d63669df8e0c8795a3558610ff
jiahao <jiahaochen535@gmail.com>
2021-05-11 19:55:05 -0700

    Fixed B

--- Lab2/ImplementationB.cpp
+++ Lab2/ImplementationB.cpp
@@ -13,17 +13,16 @@ using Lines = char[ARRAY_SIZE][16];
 
 /************* Feel free to change code below (even main() function), add functions, etc.. But do not change CL arguments *************/
 
-
-struct letter_only: std::ctype<char>
+struct letter_only : std::ctype<char>
 {
-    letter_only(): std::ctype<char>(get_table()) {}
+    letter_only() : std::ctype<char>(get_table()) {}
 
-    static std::ctype_base::mask const* get_table()
+    static std::ctype_base::mask const *get_table()
     {
         static std::vector<std::ctype_base::mask>
-                rc(std::ctype<char>::table_size,std::ctype_base::space);
+                rc(std::ctype<char>::table_size, std::ctype_base::space);
 
-        std::fill(&rc['A'], &rc['z'+1], std::ctype_base::alpha);
+        std::fill(&rc['A'], &rc['z' + 1], std::ctype_base::alpha);
         return &rc[0];
     }
 };
@@ -33,18 +32,18 @@ void DoOutput(std::string word, int result)
     std::cout << "Word Frequency: " << word << " -> " << result << std::endl;
 }
 
-
-int countFrequency( std::vector<std::string> data, std::string word)
+int countFrequency(std::vector<std::string> data, std::string word)
 {
     int freq = 0;
-    for(auto workString : data) {
-        if( !workString.compare(word) )
+    for (auto workString : data)
+    {
+        if (!workString.compare(word))
             freq++;
     }
     return freq;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     int processId;
     int numberOfProcesses;
@@ -52,14 +51,14 @@ int main(int argc, char* argv[])
     double start_time, end_time;
 
     // Setup MPI
-    MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &processId);
-    MPI_Comm_size( MPI_COMM_WORLD, &numberOfProcesses);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &processId);
+    MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
 
     // Two arguments, the program name and the input file. The second should be the input file
-    if(argc != 4)
+    if (argc != 4)
     {
-        if(processId == 0)
+        if (processId == 0)
         {
             std::cout << "ERROR: Incorrect number of arguments. Format is: <path to search file> <search word> <b1/b2>" << std::endl;
         }
@@ -69,25 +68,28 @@ int main(int argc, char* argv[])
     std::string word = argv[2];
 
     Lines lines;
-    if (processId == 0) {
+    if (processId == 0)
+    {
         std::ifstream file;
         file.imbue(std::locale(std::locale(), new letter_only()));
         file.open(argv[1]);
         std::string workString;
         int i = 0;
-        while(file >> workString){
+        while (file >> workString)
+        {
             memset(lines[i], '\0', 16);
             memcpy(lines[i++], workString.c_str(), workString.length());
         }
     }
     char buf[(ARRAY_SIZE / numberOfProcesses) * 16];
 
-    MPI_Scatter(&lines, (ARRAY_SIZE/numberOfProcesses)*16, MPI_CHAR, &buf, (ARRAY_SIZE/numberOfProcesses)*16, MPI_CHAR, 0, MPI_COMM_WORLD);
+    start_time = MPI_Wtime();
 
-    start_time=MPI_Wtime();
+    MPI_Scatter(&lines, (ARRAY_SIZE / numberOfProcesses) * 16, MPI_CHAR, &buf, (ARRAY_SIZE / numberOfProcesses) * 16, MPI_CHAR, 0, MPI_COMM_WORLD);
 
     std::vector<std::string> data;
-    for (int i = 0; i < (ARRAY_SIZE / numberOfProcesses) * 16; i += 16) {
+    for (int i = 0; i < (ARRAY_SIZE / numberOfProcesses) * 16; i += 16)
+    {
         char s[16];
         memcpy(s, &buf[i], 16);
         std::string s2(s);
@@ -97,21 +99,29 @@ int main(int argc, char* argv[])
     int temp_result = countFrequency(std::ref(data), word);
 
     std::string opt = argv[3];
-    if( !opt.compare("b1") )
+    if (!opt.compare("b1"))
     {
         std::cout << "Using reduction" << std::endl;
-        to_return = new int;
-        MPI_Reduce(&temp_result, to_return, numberOfProcesses, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    } else {
+        int temp = 0;
+        MPI_Reduce(&temp_result, &temp, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+        if (processId == 0)
+        {
+            to_return = new int(temp);
+        }
+    }
+    else
+    {
         std::cout << "Using ring" << std::endl;
         int temp;
-        if (processId == 0){
+        if (processId == 0)
+        {
             temp = temp_result;
             MPI_Send(&temp, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
             MPI_Recv(&temp, 1, MPI_INT, numberOfProcesses - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             to_return = new int(temp);
         }
-        else{
+        else
+        {
             MPI_Recv(&temp, 1, MPI_INT, processId - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             temp += temp_result;
             if (processId + 1 == numberOfProcesses)
@@ -121,14 +131,12 @@ int main(int argc, char* argv[])
         }
     }
     // ... Eventually..
-    if(processId == 0)
+    if (processId == 0)
     {
         DoOutput(word, *to_return);
-        end_time=MPI_Wtime();
-        std::cout << "time: " << ((double)end_time-start_time) << std::endl;
+        end_time = MPI_Wtime();
+        std::cout << "time: " << ((double)end_time - start_time) << std::endl;
     }
-
     MPI_Finalize();
-
     return 0;
 }
